119c1653a8e06d58ad6a6b69c0e0ceaeb316f4d1
Baha <ferchichibaha7@gmail.com>
2020-02-24 22:51:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+constructor(props){
+  super(props);
+  this.state={
+    apiResponse: ""
+  }
+}
+
+callAPI() {
+  fetch("http://localhost:9000/users")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentDidMount(){
+  this.callAPI();
+}
+
+  render() {
+    return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-intro">{this.state.apiResponse}</p>
       </header>
     </div>
-  );
+ ) };
 }
 
 export default App;
