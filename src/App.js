6b364fec133d48e6d75b34c8975b3b91a4d63a04
Baha <ferchichibaha7@gmail.com>
2020-02-25 09:20:50 +0100

    basic leagueshot

--- src/App.js
+++ src/App.js
@@ -6,19 +6,32 @@ constructor(props){
   super(props);
   this.state={
     apiResponse: "",
-    sum:'valigod'
+    gameresult:'',
+    sum:'valigod',
+    id:'',
+    isLoading: false
   }
 }
 
 callAPI=()=> {
+  this.setState({ isLoading: true });
   fetch("http://localhost:9000/users/"+this.state.sum)
       .then(res => res.json())
-      .then(res => this.setState({ apiResponse: res }))
-      .then(console.log(this.state.sum))
+      .then(res => {
+        this.setState({ apiResponse: res })
+        fetch("http://localhost:9000/users/game/"+res.id)
+         .then(res => res.json()) .then(res => {
+          this.setState({gameresult: res })
+        })
+
+
+
+      })
+      .then(() => this.setState({ isLoading: false }));
 }
 
 componentDidMount(){
-  this.callAPI("pistashio");
+  this.callAPI("valigod");
 }
 
 handleChange =(e)=>{
@@ -34,9 +47,14 @@ handleChange =(e)=>{
     <div className="App">
       <header className="App-header">
       <input className="search" type="search" placeholder="Summoner Name" onChange={this.handleChange} />
-      <button onClick={this.buttonHandler}>ok</button>
-        <p className="App-intro">{this.state.apiResponse.name}</p>
-        <p className="App-intro">{this.state.apiResponse.summonerLevel}</p>
+      <button className="btnserch" onClick={this.buttonHandler}>Go Summoner</button>
+      {this.state.isLoading ? <h3>L O A D I N G</h3>: <div> <br />
+        <img className="profileicon" alt="profileicon" src={"http://ddragon.leagueoflegends.com/cdn/10.4.1/img/profileicon/"+this.state.apiResponse.profileIconId+".png"} />
+        <h2 className="App-intro">{this.state.apiResponse.name}</h2>
+        <h5>LVL : {this.state.apiResponse.summonerLevel}</h5>
+        {this.state.gameresult.gameMode? <h5>ACTIVE GAME : {this.state.gameresult.gameMode}</h5>:null}
+       </div>}
+     
       </header>
     </div>
  ) };
