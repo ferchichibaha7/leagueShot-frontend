fcfd79566cf26f615c9a353801031e0c8d181b23
Baha <ferchichibaha7@gmail.com>
2020-02-25 03:41:47 +0100

    connect to node api

--- src/App.js
+++ src/App.js
@@ -5,26 +5,38 @@ class App extends Component {
 constructor(props){
   super(props);
   this.state={
-    apiResponse: ""
+    apiResponse: "",
+    sum:'valigod'
   }
 }
 
-callAPI() {
-  fetch("http://localhost:9000/users")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
+callAPI=()=> {
+  fetch("http://localhost:9000/users/"+this.state.sum)
+      .then(res => res.json())
+      .then(res => this.setState({ apiResponse: res }))
+      .then(console.log(this.state.sum))
 }
 
 componentDidMount(){
-  this.callAPI();
+  this.callAPI("pistashio");
 }
+
+handleChange =(e)=>{
+  this.setState({sum:e.target.value.toLowerCase()}
+  )}
+  buttonHandler=()=>{
+    this.callAPI()
+  }
 
 
   render() {
     return(
     <div className="App">
       <header className="App-header">
-        <p className="App-intro">{this.state.apiResponse}</p>
+      <input className="search" type="search" placeholder="Summoner Name" onChange={this.handleChange} />
+      <button onClick={this.buttonHandler}>ok</button>
+        <p className="App-intro">{this.state.apiResponse.name}</p>
+        <p className="App-intro">{this.state.apiResponse.summonerLevel}</p>
       </header>
     </div>
  ) };
