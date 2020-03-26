e687626fa3c192dd0ce0580f215eb51007373d47
ferchichi baha <ferchichibaha7@gmail.com>
2020-03-26 17:41:20 +0100

    first commit
    first rest api
    
    connect to node api
    
    basic leagueshot
    
    last commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+constructor(props){
+  super(props);
+  this.state={
+    apiResponse: "",
+    gameresult:'',
+    sum:'valigod',
+    id:'',
+    isLoading: false,
+    tierRes:''
+  }
+}
+
+callAPI=async()=> {
+  this.setState({ isLoading: true });
+
+let res =await fetch("http://localhost:9000/users/"+this.state.sum)
+let test= await res.json();
+this.setState({ apiResponse: test });
+let res3=await fetch("http://localhost:9000/users/rank/"+this.state.apiResponse.id)
+let rank=await res3.json();
+this.state.tierRes=rank;
+let res2=await fetch("http://localhost:9000/users/game/"+res.id)
+let activegame= await res2.json();
+this.setState({gameresult: activegame });
+
+
+this.setState({ isLoading: false })
+        
+
+
+      }
+     
+
+componentDidMount(){
+  this.callAPI("pistachioo");
+}
+
+handleChange =(e)=>{
+  this.setState({sum:e.target.value.toLowerCase()}
+  )}
+  buttonHandler=()=>{
+    this.callAPI()
+  }
+
+
+  render() {
+    return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input className="search" type="search" placeholder="Summoner Name" onChange={this.handleChange} />
+      <button className="btnserch" onClick={this.buttonHandler}>Go Summoner</button>
+      {this.state.isLoading ? <h3>L O A D I N G</h3>: <div> <br />
+        <img className="profileicon" alt="profileicon" src={"http://ddragon.leagueoflegends.com/cdn/10.4.1/img/profileicon/"+this.state.apiResponse.profileIconId+".png"} />
+        <h2 className="App-intro">{this.state.apiResponse.name}</h2>
+        <h5>LVL : {this.state.apiResponse.summonerLevel}</h5>
+        {this.state.gameresult.gameMode? <h5>ACTIVE GAME : {this.state.gameresult.gameMode}</h5>:null}
+        <img src={'./ranked-emblems/Emblem_.png'}/> 
+        <p>{this.state.tierRes.tier}</p>
+       </div>}
+     
       </header>
     </div>
-  );
+ ) };
 }
 
 export default App;
